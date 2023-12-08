9f7f14b994173d8f6d74152370935c2e5e6e78ac
Minh Nguyen <Minh.Nguyen@wisetechglobal.com>
2023-12-09 02:02:18 +1100

    task 1

--- AdventOfCode/Task1.cs
+++ AdventOfCode/Task1.cs
@@ -5,9 +5,46 @@ public class Task1
     public static void ProcessFile()
     {
         var lines = File.ReadLines("../../../task1.txt");
+
+        var sum = 0;
         foreach (var line in lines)
         {
-            Console.WriteLine(line);
+            sum += ProcessLine(line);
         }
+        
+        Console.WriteLine(sum);
     }
+
+    public static int ProcessLine(string line)
+    {
+        var firstDigit = -1;
+        for (int i = 0; i < line.Length; i++)
+        {
+            if (Char.IsDigit(line[i]) && line[i] != '0')
+            {
+                firstDigit = line[i] - '0';
+                break;
+            }
+        }
+
+        if (firstDigit < 0)
+        {
+            return 0;
+        }
+
+        var lastDigit = -1;
+        for (int i = line.Length - 1; i >= 0; i--)
+        {
+            if (Char.IsDigit(line[i]))
+            {
+                lastDigit = line[i] - '0';
+                break;
+            }
+        }
+
+        if (lastDigit < 0) return 0;
+
+        return firstDigit * 10 + lastDigit;
+    }
+
 }
