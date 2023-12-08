f16802436dfb428290181f42bddb69c0e32d62cb
Minh Nguyen <Minh.Nguyen@wisetechglobal.com>
2023-12-09 01:49:30 +1100

    initial for task1

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using AdventOfCode;
 
-Console.WriteLine("Hello, World!");
+Task1.ProcessFile();
---
+++ AdventOfCode/Task1.cs
@@ -0,0 +1,13 @@
+﻿namespace AdventOfCode;
+
+public class Task1
+{
+    public static void ProcessFile()
+    {
+        var lines = File.ReadLines("../../../task1.txt");
+        foreach (var line in lines)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
