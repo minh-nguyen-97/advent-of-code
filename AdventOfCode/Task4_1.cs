f39bd44451752fee26b5f7db00e78de8d0a82890
Minh Nguyen <Minh.Nguyen@wisetechglobal.com>
2023-12-11 03:15:32 +1100

    task4_1

---
+++ AdventOfCode/Task4_1.cs
@@ -0,0 +1,51 @@
+﻿namespace AdventOfCode;
+
+public class Task4_1
+{
+    public static void ProcessFile()
+    {
+        var lines = File.ReadLines("../../../input.txt");
+
+        var sum = 0;
+        foreach (var line in lines)
+        {
+            sum += ProcessLine(line);
+        }
+        
+        Console.WriteLine(sum);
+    }
+    
+    static int ProcessLine(string line)
+    {
+        var numbersParts = line.Split(": ")[1].Split(" | ");
+        var winningNumbers = numbersParts[0].Split(" ")
+            .Where(x => !string.IsNullOrEmpty(x) && !string.IsNullOrWhiteSpace(x))
+            .Select(x => Int32.Parse(x.Trim()))
+            .ToHashSet();
+        var owningNumbers = numbersParts[1].Split(" ")
+            .Where(x => !string.IsNullOrEmpty(x) && !string.IsNullOrWhiteSpace(x))
+            .Select(x => Int32.Parse(x.Trim()))
+            .ToArray();
+
+        var numOfOwningWinning = 0;
+        foreach (var owningNumber in owningNumbers)
+        {
+            if (winningNumbers.Contains(owningNumber))
+            {
+                numOfOwningWinning++;
+            }
+        }
+        
+        var winningPoint = 0;
+        if (numOfOwningWinning > 0)
+        {
+            winningPoint = 1;
+            for (int i = 1; i < numOfOwningWinning; i++)
+            {
+                winningPoint *= 2;
+            }
+        }
+        
+        return winningPoint;
+    }
+}
