3954bc8541e532058a22e02ad5950f759bfdc5fa
Minh Nguyen <Minh.Nguyen@wisetechglobal.com>
2023-12-12 16:40:44 +1100

    init Task12_1

---
+++ AdventOfCode/Task12_1.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCode;
+
+public class Task12_1
+{
+    public static void ProcessFile()
+    {
+        var lines = File.ReadLines("../../../input.txt");
+
+        var sum = 0;
+        foreach (var line in lines)
+        {
+            sum += ProcessLine(line);
+        }
+        
+        Console.WriteLine(sum);
+    }
+
+    static int ProcessLine(string line)
+    {
+        return 0;
+    }
+}
