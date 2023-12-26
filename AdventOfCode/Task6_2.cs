111853eb4c12357a220a54a5c62003d9609485d3
Minh Nguyen <Minh.Nguyen@wisetechglobal.com>
2023-12-26 21:23:50 +1100

    Task6_2

--- AdventOfCode/Task6_2.cs
+++ AdventOfCode/Task6_2.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode;
+﻿using System.Text;
+
+namespace AdventOfCode;
 
 public class Task6_2
 {
@@ -6,25 +8,20 @@ public class Task6_2
     {
         var lines = File.ReadLines("../../../input.txt").ToArray();
 
-        var times = ParseArray(lines[0]);
-        var distances = ParseArray(lines[1]);
+        var time = ParseNumber(lines[0]);
+        var distance = ParseNumber(lines[1]);
 
-        var result = ProcessTimeDistance(times[0], distances[0]);
-        for (int i = 1; i < times.Length; i++)
-        {
-            var numOfWays = ProcessTimeDistance(times[i], distances[i]);
-            result *= numOfWays;
-        }
+        var result = ProcessTimeDistance(time, distance);
         
         Console.WriteLine(result);
     }
 
-    static int ProcessTimeDistance(int totalTime, int goalDistance)
+    static long ProcessTimeDistance(long totalTime, long goalDistance)
     {
         var middleTime = totalTime / 2;
         if (middleTime * (totalTime - middleTime) <= goalDistance) return 0;
         
-        var (start, end) = (1, middleTime);
+        var (start, end) = ((long)1, middleTime);
         while (start <= end)
         {
             var mid = (start + end) / 2;
@@ -44,10 +41,16 @@ public class Task6_2
         return result;
     }
 
-    static int[] ParseArray(string line)
+    static long ParseNumber(string line)
     {
         var numberPart = line.Split(":")[1].Trim();
-        var numbers = numberPart.Split(" ").Where(x => !string.IsNullOrEmpty(x) && !string.IsNullOrWhiteSpace(x)).Select(x => Int32.Parse(x.Trim())).ToArray();
-        return numbers;
+        var numbers = numberPart.Split(" ").Where(x => !string.IsNullOrEmpty(x) && !string.IsNullOrWhiteSpace(x))
+        .Select(x => x.Trim());
+        var bigNumber = new StringBuilder();
+        foreach (var number in numbers)
+        {
+            bigNumber.Append(number);
+        }
+        return long.Parse(bigNumber.ToString());
     }
 }
