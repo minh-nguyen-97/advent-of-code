fc425f4bfbfec46c488e7dcf3ca610f6b2130f88
Minh Nguyen <Minh.Nguyen@wisetechglobal.com>
2023-12-26 20:47:50 +1100

    init Task6_1

---
+++ AdventOfCode/Task6_1.cs
@@ -0,0 +1,40 @@
+﻿namespace AdventOfCode;
+
+public class Task6_1
+{
+    public static void ProcessFile()
+    {
+        var lines = File.ReadLines("../../../input.txt").ToArray();
+
+        var times = ParseArray(lines[0]);
+        var distances = ParseArray(lines[1]);
+
+        var result = ProcessTimeDistance(times[0], distances[0]);
+        for (int i = 1; i < times.Length; i++)
+        {
+            var numOfWays = ProcessTimeDistance(times[i], distances[i]);
+            result *= numOfWays;
+        }
+        
+        Console.WriteLine(result);
+    }
+
+    static int ProcessTimeDistance(int totalTime, int goalDistance)
+    {
+        var count = 0;
+        for (int holdingTime = 1; holdingTime < totalTime; holdingTime++)
+        {
+            var restOfTime = totalTime - holdingTime;
+            var coveredDistance = holdingTime * restOfTime;
+            if (coveredDistance > goalDistance) count++;
+        }
+        return count;
+    }
+
+    static int[] ParseArray(string line)
+    {
+        var numberPart = line.Split(":")[1].Trim();
+        var numbers = numberPart.Split(" ").Where(x => !string.IsNullOrEmpty(x) && !string.IsNullOrWhiteSpace(x)).Select(x => Int32.Parse(x.Trim())).ToArray();
+        return numbers;
+    }
+}
